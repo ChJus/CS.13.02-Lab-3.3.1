fdb009fb253050adfd5139f4dc1b94feb685eb46
ChJus <92497757+ChJus@users.noreply.github.com>
2025-01-23 13:35:24 +0800

    finish

--- src/CISQueue.java
+++ src/CISQueue.java
@@ -1,22 +1,55 @@
 import java.util.LinkedList;
 
 public class CISQueue {
+  // LinkedList internal representation of Queue.
+  private LinkedList<Object> list;
 
-    // Linked list property.
+  // Size property.
+  private int size = 0;
 
-    // Size property.
+  // Constructor (initialize properties)
+  public CISQueue() {
+    list = new LinkedList<>();
+  }
 
-    // Constructor.
+  // Enqueue. This method adds a node to the end of the linked list.
+  public void enqueue(Object value) {
+    size++;
+    list.addLast(value);
+  }
 
-    // Enqueue. This method adds a node to the end of the linked list.
+  // Dequeue. This method removes a node from the beginning of the linked list.
+  public Object dequeue() {
+    if (isEmpty()) { // Return null if Queue is empty.
+      return null;
+    }
 
-    // Dequeue. This method removes a node from the beginning of the linked list.
+    // Decrement size count and remove first element
+    size--;
+    return list.removeFirst();
+  }
 
-    // isEmpty. Returns a boolean indicating whether the linked list is empty.
+  // isEmpty. Returns a boolean indicating whether the linked list is empty.
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-    // size. Returns the size of the queue.
+  // size. Returns the size of the queue.
+  public int size() {
+    return size;
+  }
 
-    // toString. Returns a description of the queue in, for example, the following format:
-    // CISQueue{queue=[7, 11], size=2}
+  // toString. Returns a description of the queue in, for example, the following format:
+  // CISQueue{queue=[7, 11], size=2}
+  @Override
+  public String toString() {
+    String result = "CISQueue{queue=[";
 
+    for (int i = 0; i < size; i++) {
+      result += list.get(i) + (i == size - 1 ? "" : ", ");
+    }
+
+    result += "], size=" + size + "}";
+    return result;
+  }
 }
